e2f554406651d316e8ba9166210b3734462c8717
nithyaananth <130678600+nithyaananth@users.noreply.github.com>
2023-05-26 16:46:56 +0530

    doc-1
    day1 to day15

---
+++ docs-1/day11/Day13/Dom.js
@@ -0,0 +1,23 @@
+//1.
+
+// let heading = document.querySelector('h1')
+
+// console.log(heading.textContent);
+
+
+
+//2.
+
+let paragraphs=document.querySelectorAll('p')
+
+//console.log(paragraphs.textContent);
+
+//paragraphs.forEach(para => console.log(para.textContent));
+
+//let paragraph = document.querySelectorAll('.red');
+
+//let paragraph = document.querySelectorAll('#firstDiv, p');
+
+console.log(paragraphs[0].textContent);
+
+
